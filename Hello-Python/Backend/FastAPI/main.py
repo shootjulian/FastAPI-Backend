cce276fce6a7c63b6e16e5cf1ad2deb711d5c67f
shootjulian <julian.gomez2@estudiantesunibague.edu.co>
2026-09-17 11:36:52 -0500

    Se hizo otro router de prueba para retornar un JSON

--- Hello-Python/Backend/FastAPI/main.py
+++ Hello-Python/Backend/FastAPI/main.py
@@ -6,6 +6,10 @@ app = FastAPI() #Creo un objeto "app" de la clase FastAPI
 def root():
     return "Hola, FastAPI" #Simplemente es un codigo python puro y duro
 """
-@app.get("/")
-async def root(): #Siempre que llamamos un servidor, la operacion debe ser asincrona
+@app.get("/") #Esta rotuer es el de la pagina de inicio, por asi decirlo
+async def root(): # Función asíncrona que FastAPI ejecutará cuando llegue una petición GET a "/"
     return "Hola, FastAPI"
+
+@app.get("/url")
+async def url():
+    return {"url_curso":"https://mourdev.com/python"} #Retorna un JSON
